f8822b3112286eab3b0110318ba903d93ff5e01a
1102137148 <1102137148@gm.kuas.edu.te>
2016-04-14 11:04:21 +0800

    0414

---
+++ test2/Controllers/SearchController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace test2.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+        Models.CodeService codeService = new Models.CodeService();
+        Models.OrderService orderService = new Models.OrderService();
+        /// <summary>
+        /// 訂單查詢系統
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SearchOrder()
+        {
+            ViewBag.EmpCodeData = this.codeService.GetEmp(-1);
+            ViewBag.ShipCodeData = this.codeService.GetShipper(-1);
+            return View();
+        }
+
+
+
+        [HttpPost()]
+        public ActionResult SearchOrder(Models.SearchOrderArg sorder)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            ViewBag.EmpCodeData = this.codeService.GetEmp(-1);
+            ViewBag.EmpResult = orderService.GetOrderByCondtioin(sorder);
+
+            ViewBag.ShipCodeData = this.codeService.GetShipper(-1);
+            ViewBag.ShipperResult = orderService.GetOrderByCondtioin(sorder);
+
+            Models.OrderService orderservice = new Models.OrderService();
+            List<Models.Order> result = orderservice.GetOrderByCondtioin(sorder);
+            ViewBag.searchresult = result;
+            return View("SearchOrder");
+        }
+        /// <summary>
+        /// 新增訂單畫面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public ActionResult InsertOrder()
+        {
+            ViewBag.CustCodeData = this.codeService.GetCustomer(-1);
+            ViewBag.EmpCodeData = this.codeService.GetEmp(-1);
+            ViewBag.ProductCodeData = this.codeService.GetProduct();
+            ViewBag.ShipCodeData = this.codeService.GetShipper(-1);
+            return View(new Models.Order());
+        }
+
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                Models.OrderService orderService = new Models.OrderService();
+                int a = orderService.InsertOrder(order);
+                return RedirectToAction("SearchOrder");
+
+            }
+            return View(order);
+            //return View();
+        }
+
+        /// <summary>
+        /// 更新訂單畫面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public ActionResult Update(string id)
+        {
+            Models.Order order = this.orderService.GetOrderById(id);
+            ViewBag.EmpCodeData = this.codeService.GetEmp(Convert.ToInt32(order.EmpId));
+            ViewBag.ShipCodeData = this.codeService.GetShipper(Convert.ToInt32(order.ShipperId));
+            ViewBag.CustCodeData = this.codeService.GetCustomer(Convert.ToInt32(order.CustId));
+            ViewBag.OrderDate = string.Format("{0:yyyy-MM-dd}", order.OrderDate);
+            ViewBag.RequireDdate = string.Format("{0:yyyy-MM-dd}", order.RequiredDate);
+            ViewBag.ShippedDate = string.Format("{0:yyyy-MM-dd}", order.ShippedDate);
+            ViewBag.OrderData = order;
+            return View(new Models.Order());
+        }
+
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Update(Models.Order order)
+        {
+            orderService.UpdateOrder(order);
+            ViewBag.EmpCodeData = this.codeService.GetEmp(-1);
+            ViewBag.ShipCodeData = this.codeService.GetShipper(-1);
+            return View("SearchOrder");
+        }
+
+        /// <summary>
+        /// 刪除訂單
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public JsonResult DeleteOrder(string orderId)
+        {
+            try
+            {
+                Models.OrderService orderService = new Models.OrderService();
+                orderService.DeleteOrderById(orderId);
+                return this.Json(true);
+            }
+            catch (Exception)
+            {
+
+                return this.Json(false);
+            }
+
+
+        }
+
+        /// <summary>
+        /// 取得系統時間
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetSysDate()
+        {
+            return PartialView("_SysDatePartial");
+        }
+        
+        
+    }
+}
---
+++ test2/Models/SearchOrderArg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test2.Models
+{
+    public class SearchOrderArg
+    {
+        /// <summary>
+        /// 訂單編號
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// 客戶名稱
+        /// </summary>
+        public string CustName { get; set; }
+
+        /// <summary>
+        /// 業務(員工)代號
+        /// </summary>
+        public int EmpId { get; set; }
+
+        /// <summary>
+        /// 業務(員工姓名)
+        /// </summary>
+        public string EmpName { get; set; }
+
+        /// <summary>
+        /// 出貨公司名稱
+        /// </summary>
+        public string ShipperName { get; set; }
+
+        /// <summary>
+        /// 出貨公司代號
+        /// </summary>
+        public String ShipperId { get; set; }
+
+        /// <summary>
+        /// 訂單日期
+        /// </summary>
+        public String OrderDate { get; set; }
+
+        /// <summary>
+        /// 需要日期
+        /// </summary>
+        public String RequireDdate { get; set; }
+
+        /// <summary>
+        /// 出貨日期
+        /// </summary>
+        public String ShippedDate { get; set; }
+    }
+}
